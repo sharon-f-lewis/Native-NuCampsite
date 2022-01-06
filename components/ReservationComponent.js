69193cd457c9b1415c13412ea2d99ff65935be35
Sharon Lewis <sharon.f.lewis827@gmail.com>
2022-01-05 20:35:30 -0500

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 
 class Reservation extends Component {
@@ -32,6 +33,33 @@ class Reservation extends Component {
       date: new Date(),
       showCalendar: false,
     });
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true
+        })
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`
+        },
+        trigger: null
+      });
+    }
+    let permissions = await Notifications.getPermissionsAsync();
+
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+
+    if (permissions.granted) {
+      sendNotification();
+    }
   }
 
   render () {
@@ -98,16 +126,22 @@ class Reservation extends Component {
                   'Begin Search?', 
                   'Number of Campers: ' + this.state.campers + '\n\n' +
                   'Hike-In? ' + this.state.hikeIn + '\n\n' +
-                  'Date: ' + this.state.date.toLocaleDateString, 
+                  'Date: ' + this.state.date.toLocaleDateString('en-US'), 
                   [
                     {
                       text: 'Cancel',
                       style: 'cancel',
-                      onPress: () => console.log('Cancel Pressed'),
+                      onPress: () => {
+                        console.log('Cancel Pressed');
+                        this.resetForm();
+                      }
                     },
                     {
                       text: 'OK',
-                      onPress: () => this.handleReservation()
+                      onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                      }
                     },
                   ]
                 )}
